e075416c3fa97049746356e01eaf18cb937cdc78
wangchao <wangc5@kingnet.com>
2021-02-02 14:29:08 +0800

    添加 dev.sh 脚本

--- dev.sh
+++ dev.sh
@@ -1,3 +1,32 @@
-sh build.sh
+checkNode() {
+    for line in `node -v`; do
+        echo $line
+        version=${line:1:2}
+        if [ $version -lt 15 ]
+        then
+            echo -e "jjjj"
+            return 0
+        else 
+            return 1
+        fi
+    done
+}
 
+checkNode
+ret=$?
+if [ $ret == 1 ]
+then
+    echo success
+else
+    nvm use 15.6.0
+fi
+node build_file.js
 yarn dev
+
+
+
+
+
+
+
+
