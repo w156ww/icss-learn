3b0b7436e90b82c431f56dd75197c872f8f3d69c
wangchao <wangc5@kingnet.com>
2021-02-02 11:45:41 +0800

    '多页面测试'

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,64 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const marked = require('marked');
+const marked = require("marked");
 const markdownRenderer = new marked.Renderer();
+const homePath = path.resolve(__dirname, "index.js");
+console.log("homePath: ", homePath);
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "index.js"),
-  output: {
-    filename: "[name].chunck-[fullhash:8].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: path.resolve(__dirname, "src"),
-    compress: true, // gzip 压缩
-    host: "0.0.0.0", // 设置为 0.0.0.0, 服务器外部可访问
-    port: 8081,
-    hot: true,
-    open: "http://127.0.0.1:8081",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.md$/,
-        exclude: /node_moudles/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-          {
-            loader: 'markdown-loader',
-            options: {
-              pedantic: true,
-              renderer: markdownRenderer
-            }
-          }
+    mode: "development",
+    entry: {
+        home: {
+            import: path.resolve(__dirname, "index.js"),
+        },
+        section1: {
+            import: path.resolve(__dirname, "src/section1", "index.js"),
+        },
+    },
+    output: {
+        filename: "[name].chunck-[fullhash:8].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: path.resolve(__dirname, "src"),
+        compress: true, // gzip 压缩
+        historyApiFallback: true,
+        // host: "0.0.0.0", // 设置为 0.0.0.0, 服务器外部可访问
+        port: 8081,
+        hot: true,
+        open: true,
+        inline: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.md$/,
+                exclude: /node_moudles/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                    {
+                        loader: "markdown-loader",
+                        options: {
+                            pedantic: true,
+                            renderer: markdownRenderer,
+                        },
+                    },
+                ],
+            },
         ],
-      },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "home/index.html",
+            template: path.resolve(__dirname, "", "index.html"),
+            chunks: ["home"],
+        }),
+        new HtmlWebpackPlugin({
+            filename: "section1/index.html",
+            template: path.resolve(__dirname, "src/section1", "index.html"),
+            chunks: ["section1"],
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "", "index.html"),
-    }),
-  ],
 };
